0b75ea6a815add7cc6c786bc9f2320826ea6b880
julgiraldoheinsohn <julgiraldo@heinsohn.com.co>
2022-08-10 09:35:32 -0500

    project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+    { description: 'Develop', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+
+
 }
